d08af38d9f378318fa52b3da34c691bf0a9e2b98
Jonah Miller <DrLoco3000@gmail.com>
2013-12-22 23:17:49 -0500

    extract tensor library added

---
+++ extract_kxx.py
@@ -0,0 +1,6 @@
+# extract_kxx.py
+# Author: Jonah Miller (jonah.maxwell.miller@gmail.com)
+# Time-stamp: <2013-12-22 20:17:23 (jonah)>
+
+# This program extracts the xx element of the extrinsic curvature
+# tensor of the input file.
